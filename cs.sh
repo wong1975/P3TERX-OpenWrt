e65993532a9bb3a95b35cf99bddc7366b6830197
g0g001 <g0g001@126.com>
2019-12-15 02:45:54 +0800

    Create cs.sh

---
+++ cs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#=================================================
+wget https://gitee.com/g0g001/G_RAM/raw/master/G_RAM.tar.gz
+openssl enc -e -aes256 -in G_RAM.tar.gz -out back.zip -pass pass:111111
+openssl enc -d -aes256 -in back.zip -out data.zip -pass pass:111111
+tar xzvf data.zip
+rm -rf G_RAM.tar.gz back.zip data.zip
